923e1e3a357b9c4f59f5695649cd31932a2d394d
Aryan Saxena <aryan11234567890@gmail.com>
2024-10-27 19:28:23 +0530

    First attempt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CodEasy',
+    version='0.1',
+    description='A package to auto-generate boilerplate code for common programming needs.',
+    author='Aryan Shanker Saxena',
+    author_email='aryan11234567890@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
